b41709f62c9dd8234583050da4cb41f29ed1bb44
Daniel Halasz <daniel.halasz@gmx.net>
2024-07-07 15:06:26 +0200

    Add WebSecurityConfig and SecurityFilterChain
    - Allows only authenticated clients except on
     - /
     - /login
     - /register

---
+++ src/main/java/at/technikum/springrestbackend/security/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package at.technikum.springrestbackend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
+        http
+//            .cors(AbstractHttpConfigurer::disable)
+//            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(sessionManagementCustomizer -> sessionManagementCustomizer
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .formLogin(AbstractHttpConfigurer::disable)
+            .securityMatcher("/**")
+            .authorizeHttpRequests(registry -> registry
+                    .requestMatchers("/").permitAll()
+                    .requestMatchers("/register").permitAll()
+                    .requestMatchers("/login").permitAll()
+                    .anyRequest().authenticated()
+            );
+        return http.build();
+    }
+}
